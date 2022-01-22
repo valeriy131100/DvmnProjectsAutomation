86fc475483dc2b29b52b994a79bdfc311ebef2f8
IlyaG96 <ilyag_96@icloud.com>
2022-01-22 21:12:38 +1100

    fix add_students

--- tgbot/management/commands/add_students.py
+++ tgbot/management/commands/add_students.py
@@ -3,17 +3,13 @@ from tgbot.models import Student, ProjectManager, Project, SKILL_LEVEL_CHOICES
 from django.core.management.base import BaseCommand
 
 
-def is_far_east(student):
-    return True if "True" in student else False
-
-
 def create_student(student):
     current_student, created = Student.objects.get_or_create(
         telegram_id=student['telegram_id'])
     current_student.full_name = student['name']
     current_student.skill_level = student['level']
     current_student.discord_username = student['discord_username']
-    current_student.from_far_east = (is_far_east(student['is_far_east']))
+    current_student.from_far_east = (student['is_far_east'])
     current_student.save()
 
 
